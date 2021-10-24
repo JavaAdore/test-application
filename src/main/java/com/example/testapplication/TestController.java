c1d8b61ef44d2acb78dc01773604e69281bff947
Mahmoud Eltaieb <java.adore@gmail.com>
2021-10-24 18:54:34 +0200

    Update TestController.java

--- src/main/java/com/example/testapplication/TestController.java
+++ src/main/java/com/example/testapplication/TestController.java
@@ -10,6 +10,6 @@ public class TestController {
     @RequestMapping("/test")
     public String test()
     {
-        return "hello world new Kareem";
+        return "hello world Mahmoud";
     }
 }
